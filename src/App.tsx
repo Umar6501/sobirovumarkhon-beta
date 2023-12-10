3d4f101f6c8d611a070e9fc8ad9f37e3097b44b3
Umar6501 <136899700+Umar6501@users.noreply.github.com>
2023-12-10 22:51:49 +0500

    sobirovumarkhon-beta

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,17 @@ import "./App.css";
 import Hero from "./sections/hero/Hero";
 import Load from "./components/loader/Load";
 const Header = lazy(() => import("./components/header/Header"));
-// const Hero = lazy(() => import("./pages/home/Home"));
+// const Load = lazy(() => import("./components/loader/Load"));
 // const Load = lazy(() => import("./components/loader/Load"));
 
 function App() {
   return (
     <Router>
-      <Suspense>
+      <Suspense fallback={<Load />}>
         {/* fallback={<load />} */}
         <Header />
         <Routes>
           <Route path="/" element={<Hero />} />
-          <Route path="/er" element={<Load />} />
         </Routes>
       </Suspense>
     </Router>
