902ab25a057f164ff8a830c90735064e7f3a0b88
Umar6501 <136899700+Umar6501@users.noreply.github.com>
2023-12-16 22:55:08 +0500

    sobirovumarkhon-beta

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -56,6 +56,7 @@ const Header: React.FC<Props> = () => {
               <button className={isDarkMode ? "links links2" : "links links1"}>
                 contact
               </button>
+              {/* dark mode */}
               <label
                 htmlFor="theme"
                 className={`theme ${isDarkMode ? "dark-mode" : "light-mode"}`}
@@ -80,42 +81,86 @@ const Header: React.FC<Props> = () => {
                 </span>
               </label>
             </div>
+            {/* toogle menu */}
             <div className="menu">
-              <button className="menu-btn" onClick={() => setMenu(!menu)}>
-                <input type="checkbox" id="checkbox" />
-                <label htmlFor="checkbox" className="toggle">
-                  <div
-                    className="bars"
-                    style={
-                      isDarkMode
-                        ? { backgroundColor: "white" }
-                        : { backgroundColor: "black" }
-                    }
-                    id="bar1"
-                  ></div>
-                  <div
-                    className="bars"
-                    style={
-                      isDarkMode
-                        ? { backgroundColor: "white" }
-                        : { backgroundColor: "black" }
-                    }
-                    id="bar2"
-                  ></div>
-                  <div
-                    className="bars"
-                    style={
-                      isDarkMode
-                        ? { backgroundColor: "white" }
-                        : { backgroundColor: "black" }
-                    }
-                    id="bar3"
-                  ></div>
+              <button className="menu-btn">
+                <label className="hamburger">
+                  <input
+                    className="menu-input"
+                    type="checkbox"
+                    onClick={() => setMenu(!menu)}
+                  />
+                  <svg viewBox="0 0 32 32">
+                    <path
+                      className="line line-top-bottom"
+                      style={
+                        isDarkMode ? { stroke: "white" } : { stroke: "black" }
+                      }
+                      d="M27 10 13 10C10.8 10 9 8.2 9 6 9 3.5 10.8 2 13 2 15.2 2 17 3.8 17 6L17 26C17 28.2 18.8 30 21 30 23.2 30 25 28.2 25 26 25 23.8 23.2 22 21 22L7 22"
+                    ></path>
+                    <path
+                      className="line"
+                      style={
+                        isDarkMode ? { stroke: "white" } : { stroke: "black" }
+                      }
+                      d="M7 16 27 16"
+                    ></path>
+                  </svg>
                 </label>
               </button>
             </div>
+            <div className="menu-bg"></div>
+            {/* menu list */}
+            <div
+              className="menu-list"
+              style={menu ? { right: "0%" } : { right: "-100%" }}
+            >
+              <p
+                className="menu-list-text"
+                style={
+                  isDarkMode
+                    ? { color: "white", backgroundColor: "black" }
+                    : { color: "black", backgroundColor: "white" }
+                }
+              >
+                menu list
+              </p>
+              <button className={isDarkMode ? "links links2" : "links links1"}>
+                projects
+              </button>
+              <button className={isDarkMode ? "links links2" : "links links1"}>
+                about
+              </button>
+              <button className={isDarkMode ? "links links2" : "links links1"}>
+                contact
+              </button>
+              {/* dark mode */}
+              <label
+                htmlFor="theme"
+                className={`theme ${isDarkMode ? "dark-mode" : "light-mode"}`}
+              >
+                <span className="theme__toggle-wrap">
+                  <input
+                    id="theme"
+                    className="theme__toggle"
+                    type="checkbox"
+                    role="switch"
+                    name="theme"
+                    value="dark"
+                    checked={isDarkMode}
+                    onChange={toggleTheme}
+                  />
+                  <span className="theme__fill"></span>
+                  <span className="theme__icon">
+                    {[...Array(9)].map((_, index) => (
+                      <span key={index} className="theme__icon-part"></span>
+                    ))}
+                  </span>
+                </span>
+              </label>
+            </div>
+            <div className="menu-list-left-bg"></div>
           </div>
-          {/* ///// */}
         </div>
       </div>
     </header>
